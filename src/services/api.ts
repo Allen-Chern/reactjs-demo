6818e72dfd6f42b0d9fb7cd8e4e199c721e12fe6
love_u0302@hotmail.com <Allen Chen>
2023-03-20 23:54:34 +0800

    chore: add apis

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,9 +12,21 @@ type RegisterPayload = {
 };
 
 export const sendLoginRequest = async (data: LoginPayload) => {
-  return await request.post(`/user/login`, data);
+  return await request.post(`/auth/login`, data);
+};
+
+export const sendMeRequest = async () => {
+  return await request.get("/auth/me");
 };
 
 export const sendRegisterRequest = async (data: RegisterPayload) => {
   return await request.post(`/user/register`, data);
+};
+
+export const sendVerificationRequest = async (token: string) => {
+  return await request.put(`/user/verify/${token}`);
+};
+
+export const sendResendVerificationRequest = async () => {
+  return await request.post("/user/resendVerification");
 };
