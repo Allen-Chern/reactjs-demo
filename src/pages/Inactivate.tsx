6ea31246dbfa10d202aa2622e3b006b8e205962f
love_u0302@hotmail.com <Allen Chen>
2023-03-20 23:55:38 +0800

    feat: page for resend verification

---
+++ src/pages/Inactivate.tsx
@@ -0,0 +1,43 @@
+import { Button, Container, Typography } from "@material-ui/core";
+import { AxiosResponse } from "axios";
+import { useSnackbar } from "notistack";
+import { useNavigate } from 'react-router-dom';
+import { sendResendVerificationRequest } from "../services/api";
+import { formStyles } from "../utils/form-styles";
+
+const Inactivate = () => {
+  const classes = formStyles();
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+
+  const messageHandler = (response: AxiosResponse<any,any>) => {
+    if(response.status === 400) {
+      console.log(response);
+      enqueueSnackbar(response.data.error, { variant: "error" });
+    }
+    else {
+      enqueueSnackbar('You will recieve a verification mail, please check your email inbox.', { variant: "success" });
+      navigate('/login');
+    }
+  }
+
+  const onClick = async () => {
+    const response = await sendResendVerificationRequest();
+    messageHandler(response);
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Resend Verification Mail
+        </Typography>
+        <Button type="button" variant="contained" color="secondary" onClick={onClick} fullWidth>
+            Resend
+        </Button>
+      </div>
+    </Container>
+  );
+};
+
+export default Inactivate;
